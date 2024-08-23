3023d3c54013d96f7f9d8e98a9f802d4a68929ce
haula-dv <hau.la.dv@gmail.com>
2024-08-23 12:02:30 +0700

    update test build

--- src/plugins/main-view/components/MainViewContainer.tsx
+++ src/plugins/main-view/components/MainViewContainer.tsx
@@ -3,7 +3,8 @@ import { OrderBookContainer } from '@/plugins/order-book/OrderBookContainer';
 import { TradingMainView } from '@/plugins/trading-view/components/TradingView';
 import { theme } from '@/utils';
 import { Box, Stack } from '@mui/material';
-import { Divider, SystemStatusBar } from '@orderly.network/react';
+import { AssetAndMarginSheet, Deposit, Divider, SystemStatusBar, Withdraw } from '@orderly.network/react';
+import { DepositAndWithdraw } from '@orderly.network/react/esm/block/depositAndwithdraw';
 import { CreateOrderContainer } from './create-order/CreateOrderContainer';
 import { SymbolHeader } from './SymbolHeader';
 
@@ -37,15 +38,17 @@ export const MainViewContainer = ({ onSymbolChange, symbol }: IProps) => {
 				<CreateOrderContainer symbol={symbol} />
 			</Stack>
 
-			{/* <Box px={1} bgcolor={theme.palette.background.default}>
-				<Box maxWidth={'400px'}>
-					<Deposit />
-					<AssetAndMarginSheet />
-					<Withdraw />
-					<WalletConnect status={1} />
-					<DepositAndWithdraw activeTab="deposit" />
+			<Box px={1} bgcolor={theme.palette.background.default}>
+				More
+				<Stack maxWidth={'400px'} spacing={2}>
+					<Box bgcolor={theme.palette.grey[800]}>
+						<Deposit />
 					</Box>
-			</Box> */}
+					<Withdraw />
+					<AssetAndMarginSheet />
+					<DepositAndWithdraw activeTab="deposit" />
+				</Stack>
+			</Box>
 		</Stack>
 	);
 };
