94effe5667afa39be08fcc33c5316a996e363d38
haula-dv <hau.la.dv@gmail.com>
2024-11-21 07:22:24 +0700

    fiix bug

--- src/plugins/trade/components/MainViewMobileContainer.tsx
+++ src/plugins/trade/components/MainViewMobileContainer.tsx
@@ -14,63 +14,68 @@ import OrderViewMobileContainer from "./order-view/OrderViewMobileContainer";
 import SymbolHeader from "./SymbolHeader";
 
 interface IProps {
-	symbol: string;
-	onSymbolChange: (symbol: string) => void;
+  symbol: string;
+  onSymbolChange: (symbol: string) => void;
 }
 
 const MainViewMobileContainer = ({ onSymbolChange, symbol }: IProps) => {
-	const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0);
 
-	const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-		setValue(newValue);
-	};
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
-	const tabs: ITab[] = [
-		{
-			label: "Chart",
-			value: "chart",
-			children: <TradingViewMobile onSymbolChange={onSymbolChange} symbol={symbol} />,
-		},
-		{
-			label: "Order Book",
-			value: "orderBook",
-			children: <OrderBookMobileContainer symbol={symbol} />,
-		},
-	];
+  const tabs: ITab[] = [
+    {
+      label: "Chart",
+      value: "chart",
+      children: (
+        <TradingViewMobile onSymbolChange={onSymbolChange} symbol={symbol} />
+      ),
+    },
+    {
+      label: "Order Book",
+      value: "orderBook",
+      children: <OrderBookMobileContainer symbol={symbol} />,
+    },
+  ];
 
-	return (
-		<>
-			<MarketSlider onChangeSymbol={onSymbolChange} />
+  return (
+    <>
+      <MarketSlider onChangeSymbol={onSymbolChange} />
 
-			<Divider />
+      <Divider />
 
-			<Box pl={TSizes.margin_base} pt={TSizes.margin_xs}>
-				<SymbolHeader onSymbolChange={onSymbolChange} symbol={symbol} />
-			</Box>
+      <Box pl={TSizes.margin_base} pt={TSizes.margin_xs}>
+        <SymbolHeader onSymbolChange={onSymbolChange} symbol={symbol} />
+      </Box>
 
-			<MainContainer>
-				<MainTab tabs={tabs}>
-					{/* <MainCard backgroudColor="primary" width="100%"> */}
-					<>
-						{tabs.map((item, index) => (
-							<TabPanel key={index} value={item.value} sx={{ p: 0 }}>
-								{item.children}
-							</TabPanel>
-						))}
-					</>
-					{/* </MainCard> */}
-				</MainTab>
-				<Box pt={TSizes.margin_base} />
+      <MainContainer>
+        <MainTab tabs={tabs}>
+          <>
+            {tabs.map((item, index) => (
+              <TabPanel key={index} value={item.value} sx={{ p: 0 }}>
+                {item.children}
+              </TabPanel>
+            ))}
+          </>
+        </MainTab>
+        <Box pt={TSizes.margin_base} />
 
-				<MainCard backgroudColor="primary" width="100%" minHeight="auto" disablePadding>
-					<OrderViewMobileContainer symbol={symbol} />
-				</MainCard>
-			</MainContainer>
+        <MainCard
+          backgroudColor="primary"
+          width="100%"
+          minHeight="auto"
+          disablePadding
+        >
+          <OrderViewMobileContainer symbol={symbol} />
+        </MainCard>
+      </MainContainer>
 
-			<Box py={6} />
-			<ActionPlaceOrderMobile symbol={symbol} />
-		</>
-	);
+      <Box py={6} />
+      <ActionPlaceOrderMobile symbol={symbol} />
+    </>
+  );
 };
 
 export default memo(MainViewMobileContainer);
--- src/plugins/trade/components/order-view/OrderViewMobileContainer.tsx
+++ src/plugins/trade/components/order-view/OrderViewMobileContainer.tsx
@@ -1,81 +1,119 @@
+import MainTab from "@/components/tab/MainTab";
+import TabPanel from "@mui/lab/TabPanel";
 import { Box } from "@mui/material";
-import { usePositionStream } from "@orderly.network/hooks";
-import { PositionsView } from "@orderly.network/react";
-import { DataListView } from "@orderly.network/react/esm/page/trading/desktop/sections/datalist";
+import { useOrderStream, usePositionStream } from "@orderly.network/hooks";
+import { OrdersView, PositionsView } from "@orderly.network/react";
+import { API, OrderEntity, OrderStatus } from "@orderly.network/types";
 import { useConnectWallet } from "@web3-onboard/react";
 import { memo, useState } from "react";
 
 interface IProps {
-	symbol: string;
+  symbol: string;
 }
 
 const OrderViewMobileContainer = ({ symbol }: IProps) => {
-	const [open, setOpen] = useState(true);
-	const [positions, _info, { refresh, loading }] = usePositionStream(open ? "" : symbol);
-	const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
+  const [open, setOpen] = useState(true);
+  const [positions, _info, { refresh, loading }] = usePositionStream(
+    open ? "" : symbol
+  );
+  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
 
-	const [position, setPosition] = useState<any>(null);
+  const [position, setPosition] = useState<any>(null);
 
-	const onTPSLOrder = (position: any, order: any) => {
-		setOpen(true);
-		setPosition(position);
-	};
+  const onTPSLOrder = (position: any, order: any) => {
+    setOpen(true);
+    setPosition(position);
+  };
 
-	const onShowAllSymbolChange = () => {
-		setOpen(!open);
-	};
+  const onShowAllSymbolChange = () => {
+    setOpen(!open);
+  };
 
-	const tabs = [
-		{
-			label: `Positions ${
-				positions.rows && positions.rows?.length > 0 ? `(${positions.rows?.length})` : ""
-			}`,
-			value: "positions",
-			children: (
-				<>
-					{wallet && (
-						<PositionsView
-							dataSource={positions.rows}
-							aggregated={positions.aggregated}
-							showAllSymbol={open}
-							onShowAllSymbolChange={onShowAllSymbolChange}
-						/>
-					)}
-				</>
-			),
-		},
-		{
-			label: "Pending",
-			value: "pending",
-			children: <></>,
-		},
-		{
-			label: "TP/SL",
-			value: "TP/SL",
-			children: <></>,
-		},
+  const [ordersUntyped, { isLoading }] = useOrderStream({
+    status: OrderStatus.COMPLETED,
+  });
 
-		{
-			label: "Order history",
-			value: "order_history",
-			children: <></>,
-		},
-	];
+  const orders = ordersUntyped as (API.Order | API.AlgoOrder)[];
 
-	return (
-		<Box className="data-list-view mobile" maxHeight={"400px"}>
-			<DataListView />
-			{/* <MainTab tabs={tabs}>
-				<>
-					{tabs.map((item) => (
-						<TabPanel key={item.value} value={item.value} sx={{ p: 0 }}>
-							{item.children}
-						</TabPanel>
-					))}
-				</>
-			</MainTab> */}
+  const cancelAlgoOrder = async (orderId: number, symbol: string) => {};
+  const cancelOrder = async (orderId: number, symbol: string) => {};
+  const cancelTPSLOrder = async (
+    orderId: number,
+    rootAlgoOrderId: number
+  ) => {};
+  const editAlgoOrder = async (orderId: string, order: OrderEntity) => {};
+  const editOrder = async (orderId: string, order: OrderEntity) => {};
+  const loadMore = async () => {};
 
-			{/* <Drawer anchor={"bottom"} open={open} onClose={() => setOpen(false)}>
+  const tabs = [
+    {
+      label: `Positions ${
+        positions.rows && positions.rows?.length > 0
+          ? `(${positions.rows?.length})`
+          : ""
+      }`,
+      value: "positions",
+      children: (
+        <>
+          {wallet && (
+            <PositionsView
+              dataSource={positions.rows}
+              aggregated={positions.aggregated}
+              showAllSymbol={open}
+              onShowAllSymbolChange={onShowAllSymbolChange}
+            />
+          )}
+        </>
+      ),
+    },
+    {
+      label: "Pending",
+      value: "pending",
+      children: (
+        <>
+          <OrdersView
+            cancelAlgoOrder={cancelAlgoOrder}
+            cancelOrder={cancelOrder}
+            cancelTPSLOrder={cancelTPSLOrder}
+            dataSource={orders}
+            editAlgoOrder={editAlgoOrder}
+            editOrder={editOrder}
+            isLoading={false}
+            symbol={symbol}
+            loadMore={loadMore}
+          />
+        </>
+      ),
+    },
+    {
+      label: "TP/SL",
+      value: "TP/SL",
+      children: <></>,
+    },
+
+    {
+      label: "Order history",
+      value: "order_history",
+      children: <></>,
+    },
+  ];
+
+  console.log(orders);
+
+  return (
+    <Box className="data-list-view mobile" maxHeight={"400px"}>
+      {/* <DataListView /> */}
+      <MainTab tabs={tabs}>
+        <>
+          {tabs.map((item) => (
+            <TabPanel key={item.value} value={item.value} sx={{ p: 0 }}>
+              {item.children}
+            </TabPanel>
+          ))}
+        </>
+      </MainTab>
+
+      {/* <Drawer anchor={"bottom"} open={open} onClose={() => setOpen(false)}>
 				{position && open && (
 					<ClosePositionPane
 						side={"BUY" as any}
@@ -84,8 +122,8 @@ const OrderViewMobileContainer = ({ symbol }: IProps) => {
 					/>
 				)}
 			</Drawer> */}
-		</Box>
-	);
+    </Box>
+  );
 };
 
 export default memo(OrderViewMobileContainer);
