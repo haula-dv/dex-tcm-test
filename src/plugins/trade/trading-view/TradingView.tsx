3faadb782d973df78f3e67f52ffe6623bbc37717
haula-dv <hau.la.dv@gmail.com>
2024-12-15 21:26:54 +0700

    fix feedback

--- src/plugins/trade/trading-view/TradingView.tsx
+++ src/plugins/trade/trading-view/TradingView.tsx
@@ -5,70 +5,73 @@ import dynamic from "next/dynamic";
 import { memo } from "react";
 import { OrderBookContainer } from "../order-book/OrderBookContainer";
 const SymbolOverviewNoSSR = dynamic(
-	() => import("react-ts-tradingview-widgets").then((w) => w.AdvancedRealTimeChart),
-	{
-		ssr: false,
-	},
+  () =>
+    import("react-ts-tradingview-widgets").then((w) => w.AdvancedRealTimeChart),
+  {
+    ssr: false,
+  }
 );
 interface IProps {
-	symbol: string;
-	onSymbolChange: (symbol: string) => void;
+  symbol: string;
+  onSymbolChange: (symbol: string) => void;
 }
 
 export const TradingMainView = ({ symbol, onSymbolChange }: IProps) => {
-	const theme = useTheme();
+  const theme = useTheme();
 
-	const [_, base] = symbol.split("_");
+  const [_, base] = symbol.split("_");
 
-	return (
-		<Box flex={"1 1 0%"} height={"100%"} minHeight={"480px"} display={"flex"}>
-			<Box
-				position={"relative"}
-				width={"100%"}
-				borderRadius={"14px"}
-				overflow={"hidden"}
-				bgcolor={setColorThemeMode("#fff", "#131722")}>
-				<Box
-					position={"absolute"}
-					top={0}
-					left={0}
-					border={1}
-					borderColor={setColorThemeMode("#fff", "#131722")}
-					width={"100%"}
-					height={"100%"}
-					bgcolor={"transparent"}
-					sx={{
-						pointerEvents: "none",
-						borderRadius: "14px",
-					}}
-					zIndex={9}></Box>
+  return (
+    <Box flex={"1 1 0%"} height={"100%"} minHeight={"480px"} display={"flex"}>
+      <Box
+        position={"relative"}
+        width={"100%"}
+        borderRadius={"14px"}
+        overflow={"hidden"}
+        bgcolor={setColorThemeMode("#fff", "#131722")}
+      >
+        <Box
+          position={"absolute"}
+          top={0}
+          left={0}
+          border={1}
+          borderColor={setColorThemeMode("#fff", "#131722")}
+          width={"100%"}
+          height={"100%"}
+          bgcolor={"transparent"}
+          sx={{
+            pointerEvents: "none",
+            borderRadius: "14px",
+          }}
+          zIndex={9}
+        ></Box>
 
-				<SymbolOverviewNoSSR
-					disabled_features={[
-						"hide_left_toolbar_by_default",
-						"adaptive_logo",
-						"header_chart_type",
-						"header_compare",
-						"left_toolbar",
-					]}
-					enabled_features={["header_settings", "header_chart_type"]}
-					locale="en"
-					calendar
-					theme={theme.palette.mode}
-					symbol={base}
-					autosize
-					allow_symbol_change={false}
-					interval="180"
-					range="12M"
-					timezone="Etc/UTC"
-					style="9"
-					toolbar_bg={theme.palette.primary.light}
-				/>
-			</Box>
+        <SymbolOverviewNoSSR
+          disabled_features={[
+            "hide_left_toolbar_by_default",
+            "adaptive_logo",
+            "header_chart_type",
+            "header_compare",
+            "left_toolbar",
+          ]}
+          enabled_features={["header_settings", "header_chart_type"]}
+          locale="en"
+          calendar
+          theme={theme.palette.mode}
+          symbol={`${base}USD`}
+          autosize
+          allow_symbol_change={false}
+          interval="180"
+          range="12M"
+          timezone="Etc/UTC"
+          style="1"
+          toolbar_bg={theme.palette.primary.light}
+        />
+      </Box>
 
-			<OrderBookContainer symbol={symbol} />
-		</Box>
-	);
+      <OrderBookContainer symbol={symbol} />
+    </Box>
+  );
 };
 
 export default memo(TradingMainView);
