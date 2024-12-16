f7dcc6e387e24fda8b681bde3738de908b31bcc4
haula-dv <hau.la.dv@gmail.com>
2024-12-16 16:08:58 +0700

    test wallet mobile

--- src/provider/OrderlyConfigProviderRoot.tsx
+++ src/provider/OrderlyConfigProviderRoot.tsx
@@ -7,34 +7,39 @@ import { OrderlyAppProvider } from "@orderly.network/react";
 export type NetworkId = "testnet" | "mainnet";
 
 const HostEnvMap: Record<string, ENV_NAME> = {
-	"dev-sdk-demo.orderly.network": "dev",
-	"qa-sdk-demo.orderly.network": "qa",
-	"sdk-demo-iap.orderly.network": "staging",
-	localhost: "staging",
+  "dev-sdk-demo.orderly.network": "dev",
+  "qa-sdk-demo.orderly.network": "qa",
+  "sdk-demo-iap.orderly.network": "staging",
+  localhost: "staging",
 };
 
 const OrderlyConfigProviderRoot = ({ children }: LayoutProps) => {
-	const networkId = (localStorage.getItem("networkId") ?? "mainnet") as NetworkId;
+  const networkId = (localStorage.getItem("networkId") ??
+    "mainnet") as NetworkId;
 
-	const { app } = OrderlyConfig();
+  const { app } = OrderlyConfig();
 
-	const env = networkId === "mainnet" ? "prod" : HostEnvMap[window.location.hostname] || "staging";
+  const env =
+    networkId === "mainnet"
+      ? "prod"
+      : HostEnvMap[window.location.hostname] || "staging";
 
-	const configStore = new CustomConfigStore({ networkId, env });
-	const contracts = new CustomContractManager(configStore);
+  const configStore = new CustomConfigStore({ networkId, env });
+  const contracts = new CustomContractManager(configStore);
 
-	return (
-		<OrderlyAppProvider
-			configStore={configStore}
-			networkId={networkId}
-			brokerId={app.brokerId}
-			brokerName={app.brokerName}
-			appIcons={app.appIcons}
-			shareOptions={app.shareOptions}
-			theme={"light"}>
-			{children}
-		</OrderlyAppProvider>
-	);
+  return (
+    <OrderlyAppProvider
+      configStore={configStore}
+      networkId={networkId}
+      brokerId={app.brokerId}
+      brokerName={app.brokerName}
+      appIcons={app.appIcons}
+      shareOptions={app.shareOptions}
+      theme={"light"}
+    >
+      {children}
+    </OrderlyAppProvider>
+  );
 };
 
 export default OrderlyConfigProviderRoot;
--- src/provider/WalletConnectProvider.tsx
+++ src/provider/WalletConnectProvider.tsx
@@ -1,34 +1,32 @@
 "use client";
 import { themeSelectorState } from "@/common/stores/common";
 import { OrderlyConfig } from "@/utils/config/orderly";
-import { useMediaQuery, useTheme } from "@mui/material";
 import { ConnectorProvider } from "@orderly.network/web3-onboard";
 import { useStore } from "zustand";
 import OrderlyConfigProviderRoot from "./OrderlyConfigProviderRoot";
 
 export default function Web3OnboardProviderRoot({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	const { web3Onboard } = OrderlyConfig();
-	const themeSelect = useStore(themeSelectorState, (state) => state.value);
-	const theme = useTheme();
-	const uplg = useMediaQuery(theme.breakpoints.up("lg"));
+  const { web3Onboard } = OrderlyConfig();
+  const themeSelect = useStore(themeSelectorState, (state) => state.value);
 
-	return (
-		<ConnectorProvider
-			options={
-				{
-					...web3Onboard,
-					theme: themeSelect.activeMode,
-					appMetadata: {
-						...web3Onboard.appMetadata,
-						icon: "/Orderly.svg",
-					},
-				} as any
-			}>
-			<OrderlyConfigProviderRoot>{children}</OrderlyConfigProviderRoot>
-		</ConnectorProvider>
-	);
+  return (
+    <ConnectorProvider
+      options={
+        {
+          ...web3Onboard,
+          theme: themeSelect.activeMode,
+          appMetadata: {
+            ...web3Onboard.appMetadata,
+            icon: "/Orderly.svg",
+          },
+        } as any
+      }
+    >
+      <OrderlyConfigProviderRoot>{children}</OrderlyConfigProviderRoot>
+    </ConnectorProvider>
+  );
 }
