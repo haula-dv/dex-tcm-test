cb353ba1e503ca58394ace47cbe91893348c7161
haula-dv <hau.la.dv@gmail.com>
2024-12-16 16:18:15 +0700

    fix syybol

--- src/plugins/trade/trading-view/TradingViewMobile.tsx
+++ src/plugins/trade/trading-view/TradingViewMobile.tsx
@@ -4,67 +4,70 @@ import { Box, useTheme } from "@mui/material";
 import dynamic from "next/dynamic";
 import { memo } from "react";
 const SymbolOverviewNoSSR = dynamic(
-	() => import("react-ts-tradingview-widgets").then((w) => w.AdvancedRealTimeChart),
-	{
-		ssr: false,
-	},
+  () =>
+    import("react-ts-tradingview-widgets").then((w) => w.AdvancedRealTimeChart),
+  {
+    ssr: false,
+  }
 );
 interface IProps {
-	symbol: string;
-	onSymbolChange: (symbol: string) => void;
+  symbol: string;
+  onSymbolChange: (symbol: string) => void;
 }
 
 const TradingViewMobile = ({ symbol, onSymbolChange }: IProps) => {
-	const theme = useTheme();
+  const theme = useTheme();
 
-	const [_, base] = symbol.split("_");
+  const [_, base] = symbol.split("_");
 
-	return (
-		<Box
-			position={"relative"}
-			width={"100%"}
-			borderRadius={"14px"}
-			overflow={"hidden"}
-			height={"440px"}
-			bgcolor={setColorThemeMode("#fff", "#131722")}>
-			<Box
-				position={"absolute"}
-				top={0}
-				left={0}
-				border={1}
-				borderColor={setColorThemeMode("#fff", "#131722")}
-				width={"100%"}
-				height={"100%"}
-				bgcolor={"transparent"}
-				sx={{
-					pointerEvents: "none",
-					borderRadius: "14px",
-				}}
-				zIndex={9}></Box>
+  return (
+    <Box
+      position={"relative"}
+      width={"100%"}
+      borderRadius={"14px"}
+      overflow={"hidden"}
+      height={"440px"}
+      bgcolor={setColorThemeMode("#fff", "#131722")}
+    >
+      <Box
+        position={"absolute"}
+        top={0}
+        left={0}
+        border={1}
+        borderColor={setColorThemeMode("#fff", "#131722")}
+        width={"100%"}
+        height={"100%"}
+        bgcolor={"transparent"}
+        sx={{
+          pointerEvents: "none",
+          borderRadius: "14px",
+        }}
+        zIndex={9}
+      ></Box>
 
-			<SymbolOverviewNoSSR
-				disabled_features={[
-					"hide_left_toolbar_by_default",
-					"adaptive_logo",
-					"header_chart_type",
-					"header_compare",
-					"left_toolbar",
-				]}
-				enabled_features={["header_settings", "header_chart_type"]}
-				locale="en"
-				calendar
-				theme={theme.palette.mode}
-				symbol={base}
-				autosize
-				allow_symbol_change={false}
-				interval="180"
-				range="12M"
-				timezone="Etc/UTC"
-				style="9"
-				toolbar_bg={theme.palette.primary.light}
-			/>
-		</Box>
-	);
+      <SymbolOverviewNoSSR
+        disabled_features={[
+          "hide_left_toolbar_by_default",
+          "adaptive_logo",
+          "header_chart_type",
+          "header_compare",
+          "left_toolbar",
+        ]}
+        enabled_features={["header_settings", "header_chart_type"]}
+        locale="en"
+        calendar
+        theme={theme.palette.mode}
+        symbol={`${base}USD`}
+        autosize
+        allow_symbol_change={false}
+        interval="180"
+        range="12M"
+        timezone="Etc/UTC"
+        style="9"
+        toolbar_bg={theme.palette.primary.light}
+      />
+    </Box>
+  );
 };
 
 export default memo(TradingViewMobile);
