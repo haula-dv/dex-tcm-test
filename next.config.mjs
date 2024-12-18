2fbd030c71487b030babbc68f8a743b0efa3d5c4
haula-dv <hau.la.dv@gmail.com>
2024-12-18 13:01:03 +0700

    feat: config docker

--- next.config.mjs
+++ next.config.mjs
@@ -9,70 +9,81 @@ const { __dirname, __filename } = getGlobals(import.meta.url);
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-	dest: "public",
+  dest: "public",
 });
 
 export default withPWA({
-	experimental: {
-		ppr: "incremental",
-		reactCompiler: process.env.CURRENT_ENV === "dev" ? false : true,
-	},
+  // experimental: {
+  // 	ppr: "incremental",
+  // 	reactCompiler: process.env.CURRENT_ENV === "dev" ? false : true,
+  // },
+  //   cacheHandler: require.resolve("./cache-handler.js"),
+  cacheMaxMemorySize: 0, // disable default in-memory caching
 
-	images: {
-		domains: [process.env.API_URL, process.env.IMAGE_DOMAIN],
-		unoptimized: true,
-	},
+  images: {
+    domains: [process.env.API_URL, process.env.IMAGE_DOMAIN],
+    unoptimized: true,
+  },
 
-	sassOptions: {
-		includePaths: [path.join(__dirname, "styles")],
-	},
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
 
-	env: {
-		CURRENT_ENV: process.env.CURRENT_ENV,
-		API_URL: process.env.API_URL,
-		TOKEN_API_URL: process.env.TOKEN_API_URL,
-		BROKER_ID: process.env.BROKER_ID,
-		NEXTWORK_URL: process.env.NEXTWORK_URL,
-	},
+  env: {
+    CURRENT_ENV: process.env.CURRENT_ENV,
+    API_URL: process.env.API_URL,
+    TOKEN_API_URL: process.env.TOKEN_API_URL,
+    BROKER_ID: process.env.BROKER_ID,
+    NEXTWORK_URL: process.env.NEXTWORK_URL,
+  },
 
-	reactStrictMode: false,
+  reactStrictMode: false,
 
-	typescript: {
-		// Dangerously allow production builds to successfully complete even if your project has type errors.
-		ignoreBuildErrors: true,
-	},
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if your project has type errors.
+    ignoreBuildErrors: true,
+  },
 
-	async redirects() {
-		return [
-			{
-				source: "/",
-				destination: "/trading/perp/PERP_ETH_USDC",
-				permanent: false,
-			},
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/trading/perp/PERP_ETH_USDC",
+        permanent: false,
+      },
 
-			{
-				source: "/swap",
-				destination: "/trading/perp/PERP_ETH_USDC",
-				permanent: false,
-			},
+      {
+        source: "/swap",
+        destination: "/trading/perp/PERP_ETH_USDC",
+        permanent: false,
+      },
 
-			{
-				source: "/pool",
-				destination: "/trading/perp/PERP_ETH_USDC",
-				permanent: false,
-			},
+      {
+        source: "/pool",
+        destination: "/trading/perp/PERP_ETH_USDC",
+        permanent: false,
+      },
 
-			{
-				source: "/trading/perp",
-				destination: "/trading/perp/PERP_ETH_USDC",
-				permanent: false,
-			},
+      {
+        source: "/trading/perp",
+        destination: "/trading/perp/PERP_ETH_USDC",
+        permanent: false,
+      },
 
-			{
-				source: "/trading",
-				destination: "/trading/perp/PERP_ETH_USDC",
-				permanent: false,
-			},
-		];
-	},
+      {
+        source: "/trading",
+        destination: "/trading/perp/PERP_ETH_USDC",
+        permanent: false,
+      },
+    ];
+  },
+
+  // Các tùy chọn cấu hình khác nếu cần thiết
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      // Các cấu hình riêng cho client-side (trình duyệt)
+      config.optimization.splitChunks.maxSize = 200000; // Giới hạn kích thước chunk tối đa
+    }
+    return config;
+  },
 });
