a697a05bc00cbf49b4f038039aa107b3e2e22b27
haula-dv <hau.la.dv@gmail.com>
2025-11-11 18:41:34 +0700

    Update service worker source paths and integrate Google Analytics: modify sw.js.map to reflect new file locations and enhance layout.tsx with Google Analytics scripts for improved tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import ClientThemeProvider from "@/components/ClientThemeProvider";
 import { Loading } from "@/components/loading/loading";
 import Web3OnboardProviderRoot from "@/provider/WalletConnectProvider";
+import Script from "next/script";
 import React from "react";
 import "react-toastify/dist/ReactToastify.css";
 import { Toaster } from "sonner";
@@ -12,9 +13,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head />
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-B1KWPPEGRP"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-B1KWPPEGRP');
+          `}
+        </Script>
+
         <ClientThemeProvider>
           <Web3OnboardProviderRoot>{children}</Web3OnboardProviderRoot>
 
