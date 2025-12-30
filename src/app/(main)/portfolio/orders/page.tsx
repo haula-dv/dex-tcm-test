ddbb48924d081a207747c64f03a4b0e37c73a851
haula-dv <hau.la.dv@gmail.com>
2025-12-30 17:38:58 +0700

    feat: Add portfolio section pages, new theming, and utility constants.

--- src/app/(main)/portfolio/orders/page.tsx
+++ src/app/(main)/portfolio/orders/page.tsx
@@ -1,14 +1,29 @@
-"use client";
-import { HeadPage } from "@/components/HeadPage";
-import MainOrderContainer from "@/plugins/orders/components/MainOrderContainer";
+// "use client";
+// import { HeadPage } from "@/components/HeadPage";
+// import MainOrderContainer from "@/plugins/orders/components/MainOrderContainer";
 
-const FeeTierPage = () => {
-  return (
-    <>
-      <HeadPage title="Orders" />
-      <MainOrderContainer />
-    </>
-  );
+// const FeeTierPage = () => {
+//   return (
+//     <>
+//       <HeadPage title="Orders" />
+//       <MainOrderContainer />
+//     </>
+//   );
+// };
+
+// export default FeeTierPage;
+
+import { PageTitleMap, PathEnum } from "@/utils/constant";
+import { generatePageTitle } from "@/utils/utils";
+import { Metadata } from "next";
+import dynamic from "next/dynamic";
+
+const DynamicOrdersView = dynamic(() => import("./view"));
+
+export const metadata: Metadata = {
+  title: generatePageTitle(PageTitleMap[PathEnum.Orders]),
 };
 
-export default FeeTierPage;
+export default function OrdersPage() {
+  return <DynamicOrdersView />;
+}
---
+++ src/utils/constant.ts
@@ -0,0 +1,35 @@
+export enum PathEnum {
+  Root = "/",
+  Perp = "/perp",
+
+  Portfolio = "/portfolio",
+  Positions = "/portfolio/positions",
+  Orders = "/portfolio/orders",
+  Assets = "/portfolio/assets",
+  FeeTier = "/portfolio/fee",
+  ApiKey = "/portfolio/api-key",
+  Setting = "/portfolio/setting",
+  History = "/portfolio/history",
+
+  Markets = "/markets",
+  Leaderboard = "/leaderboard",
+
+  Rewards = "/rewards",
+  RewardsTrading = "/rewards/trading",
+  RewardsAffiliate = "/rewards/affiliate",
+}
+
+export const PageTitleMap = {
+  [PathEnum.Portfolio]: "Portfolio",
+  [PathEnum.FeeTier]: "Fee tier",
+  [PathEnum.ApiKey]: "API keys",
+  [PathEnum.Orders]: "Orders",
+  [PathEnum.Assets]: "Assets",
+  [PathEnum.Positions]: "Positions",
+  [PathEnum.Setting]: "Settings",
+  [PathEnum.History]: "History",
+  [PathEnum.Markets]: "Markets",
+  [PathEnum.Leaderboard]: "Leaderboard",
+  [PathEnum.RewardsTrading]: "Trading Rewards",
+  [PathEnum.RewardsAffiliate]: "Affiliate program",
+};
