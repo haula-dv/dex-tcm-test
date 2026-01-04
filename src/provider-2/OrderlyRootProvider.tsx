3ccbea7dd1f5c9e1127283b12364fe55ea361ff8
haula-dv <hau.la.dv@gmail.com>
2026-01-04 16:07:35 +0700

    feat: Add OrderlyRootProvider to configure wallet connectors and integrate Orderly Network app provider.

--- src/provider-2/OrderlyRootProvider.tsx
+++ src/provider-2/OrderlyRootProvider.tsx
@@ -22,8 +22,8 @@ export const OrderlyRootProvider: FC<{ children: React.ReactNode }> = ({ childre
     const injected = injectedModule();
     const walletConnect = walletConnectModule({
         projectId: process.env.WALLETCONNECT_PROJECT_ID,
-        requiredChains: [10, 42161],
-        optionalChains: [421614, 11155420],
+        requiredChains: [10, 42161, 1, 8453],
+        optionalChains: [421614, 11155420, 8453],
         dappUrl: process.env.WALLETCONNECT_DAPP_URL ?? 'https://dex-tcm-test.vercel.app',
     });
     // https://dashboard.walletconnect.com/01692520-64b0-4ed8-8713-cdac19098bff/2202d3e2-19b7-4dd2-954d-3208dfdb639a
@@ -62,7 +62,7 @@ export const OrderlyRootProvider: FC<{ children: React.ReactNode }> = ({ childre
 
     const config = useOrderlyConfig()
 
-    console.log("DAPP_URL", process.env.WALLETCONNECT_DAPP_URL)
+    console.log("orderlyChains", orderlyChains)
 
     return (
         <WalletConnectorProvider
